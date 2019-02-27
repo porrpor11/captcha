cebb060b2306ee96487e5b966feadfdc35eed444
porrpor11 <kissyme.por@gmail.com>
2019-02-27 19:55:00 +0700

    commit first pass test case

--- src/main/java/com/demo/captcha/Captcha.java
+++ src/main/java/com/demo/captcha/Captcha.java
@@ -2,8 +2,53 @@ package com.demo.captcha;
 
 public class Captcha {
 
+    private int pattern;
+    private int leftOperand;
+    private int operator;
+    private int rightOperand;
+
+
+    public Captcha(int pattern, int leftOperand, int operator, int rightOperand) {
+        this.setPattern(pattern);
+        this.setLeftOperand(leftOperand);
+        this.setOperator(operator);
+        this.setRightOperand(rightOperand);
+    }
+
     @Override
     public String toString(){
         return "1+one";
+    }
+
+    public int getPattern() {
+        return pattern;
+    }
+
+    public void setPattern(int pattern) {
+        this.pattern = pattern;
+    }
+
+    public int getLeftOperand() {
+        return leftOperand;
+    }
+
+    public void setLeftOperand(int leftOperand) {
+        this.leftOperand = leftOperand;
+    }
+
+    public int getOperator() {
+        return operator;
+    }
+
+    public void setOperator(int operator) {
+        this.operator = operator;
+    }
+
+    public int getRightOperand() {
+        return rightOperand;
+    }
+
+    public void setRightOperand(int rightOperand) {
+        this.rightOperand = rightOperand;
     }
 }
--- src/test/java/com/demo/captcha/CaptchaTest.java
+++ src/test/java/com/demo/captcha/CaptchaTest.java
@@ -7,9 +7,12 @@ import static org.junit.Assert.*;
 
 public class CaptchaTest {
 
+    public CaptchaTest() {
+    }
+
     @Test
     public void firstPatternLeftOperandShouldBe1(){
-        Captcha captcha = new Captcha();
-        Assert.assertEquals("1+One",captcha.toString());
+        Captcha captcha = new Captcha(1,1,1,1);
+        Assert.assertEquals("1+one",captcha.toString());
     }
 }
